60e5c08f564e8e33bb09201bc972f0ea065f4274
zhaoyg <zhaoyg@wisevirtue.com>
2023-07-29 16:58:21 +0800

    ui

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,9 +3,21 @@ return {
          "folke/tokyonight.nvim",
          lazy = false, -- make sure we load this during startup if it is your main colorscheme
          priority = 1000, -- make sure to load this before all the other start plugins
+         dependencies = {
+             "nvim-lualine/lualine.nvim",
+             "nvim-tree/nvim-web-devicons",
+             "utilyre/barbecue.nvim",
+             "SmiteshP/nvim-navic",
+         },
          config = function()
              -- load the colorscheme here
-             vim.cmd([[colorscheme tokyonight-storm]])
+            vim.cmd([[colorscheme tokyonight-storm]])
+            require('lualine').setup{
+                theme = 'tokyonight'
+            }
+            require('barbecue').setup {
+                theme = 'tokyonight',
+            }
          end
     }
 }
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "akinsho/bufferline.nvim",
+        config = function()
+            require("bufferline").setup{}
+        end
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = true
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = true
+    },
+    {
+        "goolord/alpha-nvim",
+        config = function()
+            require 'alpha'.setup(require'alpha.themes.dashboard'.config)
+        end
+    },
+    {
+        "RRethy/vim-illuminate",
+        config = function()
+            require('illuminate').configure()
+        end
+    },
+}
